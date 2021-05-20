40f984ed2b37adfa3273705095d8fe37fd87fe0b
Yang Ricardo Barcellos Miranda <yang_ricardo@hotmail.com>
2021-05-20 13:43:05 -0300

    production fixes

--- backend/config/database.js
+++ backend/config/database.js
@@ -10,9 +10,13 @@ module.exports = ({ env }) => ({
         database: env('DATABASE_NAME', 'strapi'),
         username: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
+        ssl: env.bool('DATABASE_SSL', false) && {
+          rejectUnauthorized: false,
+        },
+      },
+      options: {
         ssl: env.bool('DATABASE_SSL', false),
       },
-      options: {}
     },
   },
 });
